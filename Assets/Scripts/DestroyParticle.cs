27489996ea7b73e2e38f120fd62d6b234699603c
n8twj <mcnamara.jeremy@gmail.com>
2014-01-19 13:49:45 -0800

    add scripts for managing the particle system(s)

---
+++ Assets/Scripts/DestroyParticle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParticle : MonoBehaviour {
+	void Start () {
+		if (gameObject.particleSystem) {
+	    	GameObject.Destroy(gameObject, gameObject.particleSystem.duration + gameObject.particleSystem.startLifetime);
+	    }
+	}
+}
---
+++ Assets/Scripts/SortingOrder.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortingOrder : MonoBehaviour {
+	void Start () {
+		particleSystem.renderer.sortingLayerName = "GUI";
+		particleSystem.renderer.sortingOrder = 2;
+	}
+}
