21c2ab7b4fb779a682018e18f2c810f1650bd598
Evan Rosson <github@erosson.org>
2014-02-28 06:18:04 -0800

    App-wide GUI skin. Doesn't do anything yet. #72

---
+++ Assets/Scripts/GUISkin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GUISkin : MonoBehaviour {
+	public UnityEngine.GUISkin skin;
+
+	void OnGUI () {
+		// Setting the skin can only be done from within onGUI, but I'd rather put it in start() so it only runs once
+		GUI.skin = skin;
+		gameObject.SetActive(false);
+	}
+}
